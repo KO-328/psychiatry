56df26b8ee7511bf6527ccfefe3634af8372170f
KO-328 <118987419+KO-328@users.noreply.github.com>
2025-06-01 22:47:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,3 +9,18 @@ setInterval(() => {
     behavior: "smooth"
   });
 }, 4000);
+
+//スマホでドロップダウン機能ができないのでタップclickで切り替えるようにする
+document.addEventListener('DOMContentLoaded', function () {
+  const navItems = document.querySelectorAll('.nav-item > a');
+
+  navItems.forEach(item => {
+    item.addEventListener('click', function (e) {
+      const dropdown = this.nextElementSibling;
+      if (dropdown && dropdown.classList.contains('dropdown')) {
+        e.preventDefault(); // 通常のリンクを無効にする
+        dropdown.classList.toggle('show'); // 表示/非表示切り替え
+      }
+    });
+  });
+});
