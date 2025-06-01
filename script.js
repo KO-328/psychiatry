2a9e7f49e15617ea5da3bf00f34fe1b3e51c64d5
KO-328 <118987419+KO-328@users.noreply.github.com>
2025-06-02 00:59:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,16 @@ function toggleMobileNav() {
   const nav = document.getElementById("mobileNav");
   nav.style.display = nav.style.display === "block" ? "none" : "block";
 }
+// ページ内リンクの際、ヘッダー分のズレ補正
+  document.addEventListener("DOMContentLoaded", function () {
+    if (location.hash) {
+      const target = document.querySelector(location.hash);
+      if (target) {
+        setTimeout(() => {
+          const offset = 100; // ← ヘッダーの高さに応じて
+          const top = target.getBoundingClientRect().top + window.scrollY - offset;
+          window.scrollTo({ top, behavior: "smooth" });
+        }, 100); // ページロード後に発動
+      }
+    }
+  });
